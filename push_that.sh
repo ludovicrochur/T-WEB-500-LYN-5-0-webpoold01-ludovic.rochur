5d928edd49f6e71815083ddcd3c57ba351e64753
Ludovic <ludovic.rochur@epitech.eu>
2020-09-14 14:50:02 +0200

    Automated commit on %d-%m-%Y %H:%M:%S

---
+++ push_that.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+REPO='/home/ludovic/delivery/T-WEB-500-LYN-5-0-webpoold01-ludovic.rochur'
+COMMIT_TIMESTAMP='%d-%m-%Y %H:%M:%S'
+GIT=`which git`
+
+if [ ! -d ${REPO}/.git ]; then
+	echo "${REPO} is not a valid git repo! Aborting..."
+	exit 0
+else
+	echo "${REPO} is a valid git repo! Proceeding..."
+fi
+
+cd ${REPO}
+${GIT} add .
+${GIT} commit -m "Automated commit on ${COMMIT_TIMESTAMP}"
+${GIT} push
